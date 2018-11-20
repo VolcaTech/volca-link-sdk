8c0f1c97f2d095f21c194194feb08031943d9046
Mikhail Dobrokhvalov <m.dobrokhvalov@gmail.com>
2018-11-21 02:44:04 +0300

    subscribtion to nft linkdrop claim events added

--- src/index.js
+++ src/index.js
@@ -1,29 +1,36 @@
 const linkUtils = require('./generateLinks');
-
+const eventListener = require('./eventListener');
+const { ABI } = require('./metadata/linkdropNFT');
 
 // library api
-const LinkGenerator = ({ verificationPK, contractAddress, networkId=1 }) => {
+const LinkSDK = ({ verificationPK, contractAddress, networkId=1, host="https://volca.app" }) => {
 
     let _verificationPK = verificationPK;
     let _contractAddress = contractAddress;
     let _networkId = networkId;
-    
+    let _host = host;
     
     const generateLinkNFT = (tokenId) => {
 	return linkUtils.generateClaimLinkNFT({
+	    tokenId,	    
 	    contractAddress: _contractAddress,
 	    verificationPK: _verificationPK,
-	    tokenId,
-	    networkId
+	    networkId: _networkId,
+	    host: _host
 	});
     };
 
+    const subscribeForClaimEventsNFT = (cb) => {
+	eventListener.subscribeForClaimEventsNFT(_contractAddress, _networkId, cb);
+    };
+    
     
     return {
-	generateLinkNFT
+	generateLinkNFT,
+	subscribeForClaimEventsNFT,
+	NFT_LINKDROP_ABI: ABI
     };
 }
 
 
-module.exports = LinkGenerator;
-
+module.exports = LinkSDK;
