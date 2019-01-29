081c544a5b2d53bb9b40852cd415ebb105296617
Mikhail Dobrokhvalov <m.dobrokhvalov@gmail.com>
2019-01-29 19:26:55 +0300

    v0.3: ERC20 links generation added

--- src/generateLinks.js
+++ src/generateLinks.js
@@ -3,7 +3,7 @@ const HOST = 'https://volca.app';
 
 
 /**
- * @desc Generate claim link.
+ * @desc Generate claim link (NFT).
  * @param  {String}  [contractAddress] - Airdrop Contract address
  * @param  {String}  [verificationPK] - Transit Private key from the URL params
  * @param  {String}  [host] - Claim Link's server host, e.g. 'https://eth2air.io'  
@@ -27,6 +27,38 @@ const generateClaimLinkNFT = ({ tokenId, verificationPK, contractAddress, host=H
     return { link, linkId: address.toLowerCase()};
 }
 
+/**
+ * @desc Generate claim link ERC20.
+ * @param  {String}  [contractAddress] - Airdrop Contract address
+ * @param  {String}  [verificationPK] - Transit Private key from the URL params
+ * @param  {String}  [host] - Claim Link's server host, e.g. 'https://eth2air.io'  
+ * @return {String}
+ */
+const generateClaimLinkERC20 = ({ verificationPK, contractAddress, host=HOST, networkId=1, referralAddress='0x0000000000000000000000000000000000000000' }) => {
+
+    // generate random key pair
+    const { address, privateKey } = utils.generateAccount();
+    
+    // sign private key with the Airdrop Transit Private Key
+    const { v, r, s } = utils.sign2Addresses({address, referralAddress, privateKey: verificationPK});
+
+    // construct link
+    let link = `${host}/#/r?v=${v}&r=${r}&s=${s}&pk=${privateKey.toString('hex')}&c=${contractAddress}`;
+    if (referralAddress !== '0x0000000000000000000000000000000000000000') {
+	link = `${link}&ref=${referralAddress}`;
+    }
+    
+    if (String(networkId) === "3") {
+	link = `${link}&n=3`;
+    }
+    
+    return {
+	link, linkId: address.toLowerCase()
+    };
+}
+
+
 module.exports = { 
-    generateClaimLinkNFT
+    generateClaimLinkNFT,
+    generateClaimLinkERC20
 }
